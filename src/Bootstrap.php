a73b3018bdfc7a46cd76babf96d8b1cacc6f654f
Rimas Rimas <mockus.ri@gmail.com>
2022-05-02 17:53:24 +0300

    first commit

---
+++ src/Bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types = 1);
+
+namespace UrlShortener;
+
+require(__DIR__ . '/../vendor/autoload.php');
+
---
+++ src/Controllers/Homepage.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types = 1);
+
+namespace UrlShortener\Controllers;
+
+class Homepage 
+{
+	
+}
