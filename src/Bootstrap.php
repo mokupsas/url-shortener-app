397d5b15d6543b50fbe1d1efa8c6f20f7e8d2e33
Rimas Rimas <mockus.ri@gmail.com>
2022-05-02 18:17:27 +0300

    added Routing

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -3,5 +3,50 @@ declare(strict_types = 1);
 
 namespace UrlShortener;
 
+use Http\HttpRequest;
+use Http\HttpResponse;
+
 require(__DIR__ . '/../vendor/autoload.php');
 
+$request = new HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER, file_get_contents('php://input'));
+$response = new HttpResponse();
+
+/*
+ *	Managing routes
+ */
+$dispatcher = \FastRoute\simpleDispatcher(function(\FastRoute\RouteCollector $r) {
+	$routes = require(__DIR__ . '/Routes.php');
+	
+	foreach($routes as $route)
+	{
+		$r->addRoute($route[0], $route[1], $route[2]);
+	}
+});
+
+$routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getUri());
+switch($routeInfo[0]){
+    case \FastRoute\Dispatcher::NOT_FOUND:
+        // ... 404 Not Found
+        break;
+    case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        // ... 405 Method Not Allowed
+        break;
+    case \FastRoute\Dispatcher::FOUND:
+        $className = $routeInfo[1][0];
+        $method = $routeInfo[1][1];
+        $vars = $routeInfo[2];
+        // ... call $handler with $vars
+		echo '1';
+        break;
+}
+
+
+/*
+ *	Giving website output 
+ */
+foreach($response->getHeaders() as $header)
+{
+	header($header, false);
+}
+echo $response->getContent();
